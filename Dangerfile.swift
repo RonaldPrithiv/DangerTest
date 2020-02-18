5566eae3f4c41f85c6cb28e0ea02c011e10e0619
RonaldPrithiv <32409931+RonaldPrithiv@users.noreply.github.com>
2020-02-18 15:35:52 +0400

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,7 +1,6 @@
 import Danger 
 let danger = Danger()
 
-string.joined(separator: "\",\"")
 fail("Highlight something in the table")
 message(danger.git.deletedFiles.joined(separator: " "))
 message(danger.git.modifiedFiles.joined(separator: " "))
