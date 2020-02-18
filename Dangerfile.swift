58bddd2f0044d9a8cf71e47d991da616eccc3681
RonaldPrithiv <32409931+RonaldPrithiv@users.noreply.github.com>
2020-02-18 15:13:12 +0400

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,4 +2,7 @@ import Danger
 let danger = Danger()
 
 fail("Highlight something in the table")
-message("Something pretty bad, but not important enough to fail the build")
+message(git.deleted_files)
+message(git.modified_files)
+message(git.added_files)
+
